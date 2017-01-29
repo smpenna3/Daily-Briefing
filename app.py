5bdd28940f8bccdec6279d2f8ea29250b27cc0ce
z0glen <zgbauer@gmail.com>
2017-01-29 09:35:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,6 @@ from datetime import datetime
 import os
 
 app = Flask(__name__)
-db_path = os.path.join(os.path.dirname(__file__), 'app.db')
-db_uri = 'sqlite:///{}'.format(db_path)
-app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-app.config['SECRET_KEY'] = 'mchacks demo zgb 0001'
 
 db = SQLAlchemy(app)
 api = Api(app)	
