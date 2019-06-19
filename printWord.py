3ea3a9c9b29244737ff275c8a25ce44c12999bfd
Seth <smpenna3@gmail.com>
2019-06-19 14:30:38 -0400

    Added adafruit library

--- printWord.py
+++ printWord.py
@@ -1,3 +1,5 @@
+## Python3 only!
+
 import serial as serial
 import requests
 import json
@@ -6,7 +8,8 @@ import time
 
 # Printer will print 32 wide
 def printer(d):
-	ser = serial.Serial('/dev/ttyACM0', 9600)
+	ser = serial.Serial('/dev/ttyUSB0', 9600)
+	time.sleep(2)
 	timeToPrint = int(float(len(d)) / 32.0)+3
 	currentLength = 0
 	totalLength = 0
@@ -34,16 +37,14 @@ def printer(d):
 
 	totalLength -= 4
 
-	ser.write(final)
+	ser.write(final.encode())
 	time.sleep(timeToPrint)
+	print(final)
+	print(final.encode())
 
-d = ''
+if __name__ == '__main__':
+	d = ''
 
-while(d != 'exit'):
-	a = raw_input('print: ')
-	b = a + '|'
-
-	c = b.decode("utf-8")
-	d = c.encode("ascii", "ignore")
-
-	printer(d)
+	while(d != 'exit'):
+		a = input('print: ')
+		printer(a+'|')
