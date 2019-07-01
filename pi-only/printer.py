d32487858062aabeb4f1c3094b7b0ad736efe22a
smpenna3 <smpenna3@gmail.com>
2019-07-01 09:14:31 -0400

    Small fixes

--- pi-only/printer.py
+++ pi-only/printer.py
@@ -5,7 +5,7 @@ Requires the printer_wrapper python2 file
 
 import subprocess
 
-def print_text(text, size='medium', bold=False, inverse=False, tall=False, underline=False, feed=3, justify='left'):
+def print_text(text, size='small', bold=False, inverse=False, tall=False, underline=False, feed=3, justify='left'):
     # Generate the base call
     call = 'python2 printer_wrapper.py "' + text + '"'
 
@@ -26,9 +26,9 @@ def print_text(text, size='medium', bold=False, inverse=False, tall=False, under
     call += ' -f ' + str(feed)
 
     # Check for justify
-    if(justfy not in ['left', 'right', 'center', 'l', 'r', 'c']):
+    if(justify not in ['left', 'right', 'center', 'l', 'r', 'c']):
         raise ValueError("Invalid justification option: " + str(justify))
     call += ' -j ' + justify
 
     # Send call
-    subprocess.Popen(call.split())
+    subprocess.Popen(call)
