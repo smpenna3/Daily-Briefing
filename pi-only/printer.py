6b0d00d3a80d5167a79434ae664f8cc0ab71dc8f
smpenna3 <smpenna3@gmail.com>
2019-07-01 09:08:26 -0400

    Added python3 library

---
+++ pi-only/printer.py
@@ -0,0 +1,32 @@
+'''
+Python3 library for the Adafruit Thermal Receipt Printer
+Requires the printer_wrapper python2 file
+'''
+
+import subprocess
+
+def print_text(text, size='medium', bold=False, inverse=False, tall=False, underline=False, feed=3, justify='left'):
+    # Generate the base call
+    call = 'python2 printer_wrapper.py "' + text + '"'
+
+    # Check for size change
+    if(size not in ['large', 'medium', 'small']):
+        raise ValueError("Invalid size option: " + str(size))
+    call += ' --' + size
+
+    # Check for formatting
+    if(bold): call += ' -b'
+    if(inverse): call += ' -i'
+    if(tall): call += ' -t'
+    if(underline): call += ' -u'
+
+    # Check for feed
+    call += ' -f ' + int(feed)
+
+    # Check for justify
+    if(justfy not in ['left', 'right', 'center', 'l', 'r', 'c']):
+        raise ValueError("Invalid justification option: " + str(justify))
+    call += ' -j ' + justify
+
+    # Send call
+    subprocess.Popen(call.split())
